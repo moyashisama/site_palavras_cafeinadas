ea89cd90d8641a743ca235aea4e89cd7dc8f69e5
fabiosato0 <hidekif620@gmail.com>
2024-05-20 00:03:37 -0300

    mais progresso

---
+++ js/slide-script.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+      0: {
+        slidesPerView:1,
+      },
+      520: {
+        slidesPerView:2,
+      },
+      950: {
+        slidesPerView:3,
+      },
+    },
+});
